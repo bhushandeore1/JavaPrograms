2280c825531768f1f3bb14b0aeb56073ded9483c
HP <HP@DESKTOP-1VA4OFU>
2023-05-21 13:07:34 +0530

    FirsCommit

---
+++ JavaProgramsPractice/src/Bhushan/PeakIndexinaMountainArray.java
@@ -0,0 +1,15 @@
+package Bhushan;
+
+//
+//Input: arr = [0,1,0]
+//Output: 1
+/*Input: arr = [0,2,1,0]
+Output: 1*/
+public class PeakIndexinaMountainArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
