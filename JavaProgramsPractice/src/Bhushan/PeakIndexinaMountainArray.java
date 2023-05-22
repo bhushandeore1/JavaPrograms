d77ea0b9761f6cef911172916803130d459d6cb0
bhushandeore1 <pbhushandeore@gmail.com>
2023-05-23 01:23:42 +0530

    PeakIndexIn MountainArray

--- JavaProgramsPractice/src/Bhushan/PeakIndexinaMountainArray.java
+++ JavaProgramsPractice/src/Bhushan/PeakIndexinaMountainArray.java
@@ -8,8 +8,24 @@ Output: 1*/
 public class PeakIndexinaMountainArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int []arr= {0,1,0};
+		System.out.println(findpeak(arr));
+	}
 
+	private static int findpeak(int[] arr) {
+		int start=0;
+		int end=arr.length-1;
+		while(start<end) {
+			int mid=start+(end-start)/2;
+			if(arr[mid]>arr[mid+1]) {
+				end=mid;
+			}
+			else {
+				start=mid+1;
+			}
+		}
+		return start;
+		
 	}
 
 }
